4d7414e89fdfe08e9a8b56bf3735f827eaac860d
NeilSCGH <neil.segard@gmail.com>
2020-10-19 11:15:52 +0200

    value iteration ok

--- 3. Reinforcement Learning/hw2.py
+++ 3. Reinforcement Learning/hw2.py
@@ -25,9 +25,6 @@ R[0,3] = +1
 R[1,3] = -1
 R[1,1] = -10
 
-print(R)
-print()
-
 #Making policy (where to go for each state)
 policy = np.random.randint(0,4,(3, 4))#random juste pour les tests faudra calculer
 
@@ -36,25 +33,29 @@ def newState(originRow, originCol, action):
     row = originRow
 
     if action == 0: #North
-        row = originRow + 1
-    elif action == 1: #South
         row = originRow - 1
+    elif action == 1: #South
+        row = originRow + 1
     elif action == 2: #East
         col = originCol + 1
     elif action == 3: #West
         col = originCol - 1
-
 
     if row > 2 : row = 2
     if row < 0 : row = 0
     if col > 3 : col = 3
     if col < 0 : col = 0
 
+    if row ==1 and col == 1:
+        col = originCol
+        row = originRow
+
     return row, col
 
 def possibleStates(row, col):
     list=[]
 
+    #Actions : N=0, S=1, E=2, W=3
     for action in range(4):
         list.append(newState(row, col, action))
 
@@ -63,55 +64,86 @@ def possibleStates(row, col):
 ##Value iteration
 values=np.zeros((3, 4))
 valuesFixed=[(0,3),(1,3),(1,1)]
+values[0,3]=+1
+values[1,3]=-1
+values[1,1]=-10
+valuesOld=np.ones((3, 4))
 
-print("Values")
-print(values)
-print()
-
-
-for rep in range(5):
+#Finding optimal Value function
+while np.sum(abs(valuesOld-values)>0.00001) != 0 :
+    valuesOld=np.copy(values)
     for col in range(4):
         for row in range(3):
             if (row,col) not in valuesFixed:
                 r = R[row][col]
 
-                #pour chaque action
-                #Actions : N=0, S=1, E=2, W=3
+                caseNorth, caseSouth, caseEast, caseWest = possibleStates(row, col)
+
                 pList=[]
-                for action in range(4):
-                    caseNorth, caseSouth, caseEast, caseWest = possibleStates(row, col)
+                #North
+                pList.append(0.8 * values[caseNorth[0],caseNorth[1]] \
+                           + 0.1 * values[caseEast[0],caseEast[1]] \
+                           + 0.1 * values[caseWest[0],caseWest[1]])
 
-                    if action == 0: #North
-                        p = 0.8 * values[caseNorth[0],caseNorth[1]] \
+                #Sud
+                pList.append(0.8 * values[caseSouth[0],caseSouth[1]] \
+                           + 0.1 * values[caseEast[0],caseEast[1]] \
+                           + 0.1 * values[caseWest[0],caseWest[1]])
+
+                #East
+                pList.append(0.8 * values[caseEast[0],caseEast[1]] \
+                           + 0.1 * values[caseNorth[0],caseNorth[1]] \
+                           + 0.1 * values[caseSouth[0],caseSouth[1]])
+
+                #West
+                pList.append(0.8 * values[caseWest[0],caseWest[1]] \
+                           + 0.1 * values[caseNorth[0],caseNorth[1]] \
+                           + 0.1 * values[caseSouth[0],caseSouth[1]])
+
+                maxi = np.max(pList)
+
+                values[row][col] = r + gamma * maxi
+
+
+
+print("Values")
+print(values)
+print()
+
+#Finding optimal Policy
+policy=np.ones((3, 4))*-1
+
+for col in range(4):
+    for row in range(3):
+        if (row,col) not in valuesFixed:
+            caseNorth, caseSouth, caseEast, caseWest = possibleStates(row, col)
+
+            pList=[]
+            #North
+            pList.append(0.8 * values[caseNorth[0],caseNorth[1]] \
                         + 0.1 * values[caseEast[0],caseEast[1]] \
-                        + 0.1 * values[caseWest[0],caseWest[1]]
-                    elif action == 1: #Sud
-                        p = 0.8 * values[caseSouth[0],caseSouth[1]] \
+                        + 0.1 * values[caseWest[0],caseWest[1]])
+
+            #Sud
+            pList.append(0.8 * values[caseSouth[0],caseSouth[1]] \
                         + 0.1 * values[caseEast[0],caseEast[1]] \
-                        + 0.1 * values[caseWest[0],caseWest[1]]
-                    elif action == 2: #East
-                        p = 0.8 * values[caseEast[0],caseEast[1]] \
+                        + 0.1 * values[caseWest[0],caseWest[1]])
+
+            #East
+            pList.append(0.8 * values[caseEast[0],caseEast[1]] \
                         + 0.1 * values[caseNorth[0],caseNorth[1]] \
-                        + 0.1 * values[caseSouth[0],caseSouth[1]]
-                    elif action == 3: #West
-                        p = 0.8 * values[caseWest[0],caseWest[1]] \
+                        + 0.1 * values[caseSouth[0],caseSouth[1]])
+
+            #West
+            pList.append(0.8 * values[caseWest[0],caseWest[1]] \
                         + 0.1 * values[caseNorth[0],caseNorth[1]] \
-                        + 0.1 * values[caseSouth[0],caseSouth[1]]
+                        + 0.1 * values[caseSouth[0],caseSouth[1]])
 
-                    pList.append(p)
+            policy[row][col] = np.argmax(pList)
 
-                max = np.max(pList)
-
-                values[row][col] = r + gamma * max
-
-    print(values)
-    print()
-
-
-
-
-
-
+print("Policy")
+print("Actions : N=0, S=1, E=2, W=3")
+print(policy)
 
 
 
