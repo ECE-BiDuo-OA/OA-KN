66682b4c3d030e2899a41eefd860949754473e71
NeilSCGH <neil.segard@gmail.com>
2020-11-14 11:13:43 +0100

    q2 ok

--- 6. Deep Learning Software - TensorFlow/main.py
+++ 6. Deep Learning Software - TensorFlow/main.py
@@ -17,6 +17,26 @@ t=my_data["t"]
 z=my_data["z"]
 
 ##Q1 Plotting data
-plt.plot(t,z)
-plt.show()
+#plt.plot(t,z)
+#plt.show()
 
+##Q2 Splitting the data
+K = 10
+N = 150
+J = 1
+
+X = []
+Y = []
+
+for i in range(len(z) - N - (J-1)):
+    X.append(z[i: i + N])
+    Y.append(z[i + N: i + N + J])
+
+
+I = len(X) - K #number of training set
+
+Xtrain=np.asarray(X[:I])
+Ytrain=np.asarray(Y[:I])
+
+Xtest=np.asarray(X[I:])
+Ytest=np.asarray(Y[I:])
